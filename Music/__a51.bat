1739b470072d2c3813c714993d5f45051a35530a
AmanPrakash09 <89485425+AmanPrakash09@users.noreply.github.com>
2023-08-30 02:07:41 -0700

    Add files via upload

---
+++ Music/__a51.bat
@@ -0,0 +1,5 @@
+@echo off
+C:
+cd "\Users\alexj\Documents\ELEC2\ELEC291\Music\"
+"C:\Users\alexj\Documents\ELEC2\ELEC291\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\alexj\Documents\ELEC2\ELEC291\Music\Music.asm"
+echo Crosside_Action Set_Hex_File C:\Users\alexj\Documents\ELEC2\ELEC291\Music\Music.HEX
